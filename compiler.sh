1e582741211676e283eb1961a777c0a22458b5fa
garamonfok <francois@barrabin.org>
2011-10-14 13:23:56 +0200

    clean

--- compiler.sh
+++ compiler.sh
@@ -15,7 +15,12 @@ pdflatex -interaction=nonstopmode --src-specials main
 
 cd ..
 
-rm -f tex_source/*.aux tex_source/*.bbl tex_source/*.ilg tex_source/*.lot tex_source/*.nlo tex_source/*.nls tex_source/*.toc tex_source/*.out tex_source/*.lof tex_source/*.blg tex_source/*.log tex_source/*.mtc* tex_source/*.idx tex_source/*.maf
+rm -f tex_source/*.aux tex_source/*.bbl tex_source/*.ilg tex_source/*.lot 
+rm -f tex_source/*.nlo tex_source/*.nls tex_source/*.toc tex_source/*.out 
+rm -f tex_source/*.lof tex_source/*.blg tex_source/*.log tex_source/*.mtc* 
+rm -f tex_source/*.idx tex_source/*.maf tex_source/*.plf tex_source/*.plt
+rm -f tex_source/*.plc
+
 rm -f tex_source/*/*.aux tex_source/*/*/*.aux
 
 mv tex_source/main.pdf .
